fb3ce481259293927568395a525ab16681cd566b
JazminOballos <jazminoballos55@gmail.com>
2022-01-31 20:03:33 -0300

    puerto

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 const publicPath=path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.get('/', (req,res) =>{
+/*app.get('/', (req,res) =>{
     res.send ('Bienvenido');
-});
+});*/
 
 app.get('/mercado_liebre', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html')); 
