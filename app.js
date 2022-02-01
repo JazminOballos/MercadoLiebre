85a2223e354bd367918d458d7ffff6ce4b9191d2
JazminOballos <jazminoballos55@gmail.com>
2022-01-31 21:12:33 -0300

    arregle app.js

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express();
 const publicPath=path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-/*app.get('/', (req,res) =>{
-    res.send ('Bienvenido');
-});*/
+app.listen(process.env.PORT || 3000, function (){
+    console.log('servidor corriendo')
+});
 
-app.get('/mercado_liebre', (req,res) =>{
+app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html')); 
 });
 
@@ -27,6 +27,4 @@ app.get('*', (req,res) =>{
     res.send("Lo siento, no puedes acceder");
 });
 
-app.listen(process.env.PORT || 3000, function (){
-    console.log('servidor corriendo')
-});
+
